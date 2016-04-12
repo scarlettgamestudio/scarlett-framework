6567d5dc8582acd938dfca178cc2a5fb5c7b4148
João Alves <joao.cpp.sca@gmail.com>
2016-04-12 18:00:12 +0100

    % base commit

---
+++ src/gameScene.js
@@ -0,0 +1,17 @@
+/**
+ * GameScene class
+ */
+var GameScene = (function() {
+
+    /**
+     * @constructor
+     */
+    function GameScene(params) {
+        params = params || {};
+
+        this.name = params.name || "GameScene";
+    }
+
+    return GameScene;
+
+})();
