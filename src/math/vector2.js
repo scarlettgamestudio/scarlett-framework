a4740832c9437ef8353461d3cd019dd706da205a
Luís Fernandes <luisapidcloud@hotmail.com>
2016-08-28 12:27:50 +0100

    + Vector cross product (2 dimensions) + some comments

--- src/math/vector2.js
+++ src/math/vector2.js
@@ -37,8 +37,24 @@ Vector2.prototype.normalRight = function () {
     return new Vector2(-1 * this.y, this.x);
 };
 
+/**
+ * The dot product of this vector with another vector.
+ * @param vector
+ * @returns {number}
+ */
 Vector2.prototype.dot = function (vector) {
     return this.x * vector.x + this.y * vector.y;
+};
+
+/**
+ * Calculates the magnitude of the vector that would result from a regular 3D cross product of the input vectors,
+ * taking their Z values implicitly as 0 (i.e., treating the 2D space as a plane in the 3D space).
+ * The 3D cross product will be perpendicular to that plane, and thus have 0 X & Y components
+ * (thus the scalar returned is the Z value of the 3D cross product vector).
+ * @param vector
+ */
+Vector2.prototype.cross = function (vector) {
+    return this.x * vector.y - this.y * vector.x;
 };
 
 Vector2.prototype.multiply = function (vector) {
@@ -62,12 +78,24 @@ Vector2.restore = function (data) {
     return new Vector2(data.x, data.y);
 };
 
+/**
+ * The distance between the points represented by VectorA and VectorB
+ * @param vectorA
+ * @param vectorB
+ * @returns {number}
+ */
 Vector2.distance = function (vectorA, vectorB) {
     var v1 = vectorA.x - vectorB.x;
     var v2 = vectorA.y - vectorB.y;
     return Math.sqrt((v1 * v1) + (v2 * v2));
 };
 
+/**
+ * The squared distance between the points represented by VectorA and VectorB
+ * @param vectorA
+ * @param vectorB
+ * @returns {number}
+ */
 Vector2.sqrDistance = function (vectorA, vectorB) {
     var v1 = vectorA.x - vectorB.x;
     var v2 = vectorA.y - vectorB.y;
