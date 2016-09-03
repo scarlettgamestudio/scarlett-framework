a98cef548611ef92c799b0d20eccea45586cf0d1
João Alves <joao.cpp.sca@gmail.com>
2016-09-03 03:32:11 +0100

    % grid extension optimization

--- build/scarlett-framework.js
+++ build/scarlett-framework.js
@@ -11155,10 +11155,12 @@ Sprite.prototype.setTextureSrc = function (path) {
         Texture2D.fromPath(path).then(
             (function (texture) {
                 this.setTexture(texture);
-            }).bind(this), function (error) {
-                // TODO: log this..
-            }
+            }).bind(this), (function (error) {
+                this.setTexture(null);
+            }).bind(this)
         );
+    } else {
+        this.setTexture(null);
     }
 };
 
@@ -11716,14 +11718,16 @@ GridExt.prototype.render = function (delta) {
         // I have an idea that can be great here..
         // create a global event for whenever the camera properties change (aka, calculate matrix is called), and store
         // the following calculations on event:
-        var screenResolution = this._game.getVirtualResolution();
-        var offsetX = this._game.getActiveCamera().x - (this._game.getActiveCamera().x % this._gridSize);
-        var offsetY = this._game.getActiveCamera().y - (this._game.getActiveCamera().y % this._gridSize);
         var zoom = this._game.getActiveCamera().zoom;
+        var floorZoom = Math.floor(zoom);
+        var gridSize = floorZoom > 1 ? this._gridSize * floorZoom : this._gridSize;
+        var screenResolution = this._game.getVirtualResolution();
+        var offsetX = this._game.getActiveCamera().x - (this._game.getActiveCamera().x % gridSize);
+        var offsetY = this._game.getActiveCamera().y - (this._game.getActiveCamera().y % gridSize);
         var zoomDifX = (zoom * screenResolution.width) * 2.0;
         var zoomDifY = (zoom * screenResolution.height) * 2.0;
-        var howManyX = (screenResolution.width + zoomDifX) / this._gridSize + 2;
-        var howManyY = (screenResolution.height + zoomDifY) / this._gridSize + 2;
+        var howManyX = (screenResolution.width + zoomDifX) / gridSize + 2;
+        var howManyY = (screenResolution.height + zoomDifY) / gridSize + 2;
         var left = -(screenResolution.width + zoomDifX) / 2;
         var right = (screenResolution.width + zoomDifX) / 2;
         var top = -(screenResolution.height + zoomDifY) / 2;
@@ -11733,23 +11737,32 @@ GridExt.prototype.render = function (delta) {
         for (var x = 0; x < howManyX; x++) {
             this._primitiveRender.drawLine(
                 {
-                    x: x * this._gridSize + left - (left % this._gridSize) + offsetX,
-                    y: bottom + this._gridSize + offsetY
+                    x: x * gridSize + left - (left % gridSize) + offsetX,
+                    y: bottom + gridSize + offsetY
                 },
-                {x: x * this._gridSize + left - (left % this._gridSize) + offsetX, y: top - this._gridSize + offsetY},
+                {
+                    x: x * gridSize + left - (left % gridSize) + offsetX,
+                    y: top - gridSize + offsetY
+                },
                 1, this._gridColor);
         }
 
         // vertical shift _ _ _ _ _
         for (var y = 0; y < howManyY; y++) {
             this._primitiveRender.drawLine(
-                {x: right + this._gridSize + offsetX, y: y * this._gridSize + top - (top % this._gridSize) + offsetY},
-                {x: left - this._gridSize + offsetX, y: y * this._gridSize + top - (top % this._gridSize) + offsetY},
+                {
+                    x: right + this._gridSize + offsetX,
+                    y: y * gridSize + top - (top % gridSize) + offsetY
+                },
+                {
+                    x: left - gridSize + offsetX,
+                    y: y * gridSize + top - (top % gridSize) + offsetY
+                },
                 1, this._gridColor);
         }
 
         // main "lines" (origin)
-        if (this._originLines) {
+        if (this._originLines && floorZoom < 2) {
             // vertical
             this._primitiveRender.drawRectangle(
                 new Rectangle(-2, top - this._gridSize + offsetY, 4, screenResolution.height + zoomDifY),
