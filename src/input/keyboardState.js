9442202fedea0bde4066904506381880e1755cf7
João Alves <joao.cpp.sca@gmail.com>
2016-09-03 16:05:26 +0100

    % keyboard state improvement

--- src/input/keyboardState.js
+++ src/input/keyboardState.js
@@ -3,20 +3,28 @@
  * @param keys
  * @constructor
  */
-function KeyboardState (keys) {
+function KeyboardState(keys) {
     // now we copy the values to our state array.
     this._keys = [];
     keys.forEach((function (key) {
-       this._keys.push(key);
+        this._keys.push(key);
     }).bind(this));
 }
+
+/**
+ * Gets the keys currently being pressed
+ * @returns {Array}
+ */
+KeyboardState.prototype.getKeys = function () {
+    return this._keys;
+};
 
 /**
  * Gets if the given key is currently being pressed
  * @param key
  * @returns {boolean}
  */
-KeyboardState.prototype.isKeyDown = function(key) {
+KeyboardState.prototype.isKeyDown = function (key) {
     return this._keys.indexOf(key) >= 0;
 };
 
@@ -25,6 +33,6 @@ KeyboardState.prototype.isKeyDown = function(key) {
  * @param key
  * @returns {boolean}
  */
-KeyboardState.prototype.isKeyUp = function(key) {
+KeyboardState.prototype.isKeyUp = function (key) {
     return this._keys.indexOf(key) < 0;
 };
