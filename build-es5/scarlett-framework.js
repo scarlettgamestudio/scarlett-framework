0b577ceba909cc86c7ba54a7ac1a2b5bec4488d6
Luís Fernandes <luisapidcloud@hotmail.com>
2017-02-15 23:35:49 +0000

    update: PrimitiveBatch to ES6

--- build-es5/scarlett-framework.js
+++ build-es5/scarlett-framework.js
@@ -13994,92 +13994,124 @@ GameScene.restore = function (data) {
 GameScene.prototype.unload = function () {};; /**
                                               * PrimitiveBatch class for on demand direct drawing
                                               */
-function PrimitiveBatch(game) {
-    if (!isGame(game)) {
-        throw error("Cannot create primitive render, the Game object is missing from the parameters");
-    }
 
-    // public properties:
+var PrimitiveBatch = function () {
 
+    //#region Constructors
 
-    // private properties:
-    this._game = game;
-    this._gl = game.getRenderContext().getContext();
-    this._primitiveShader = new PrimitiveShader();
-    this._vertexBuffer = this._gl.createBuffer();
-    this._colorBuffer = this._gl.createBuffer();
+    /**
+     *
+     * @param game
+     */
+    function PrimitiveBatch(game) {
+        _classCallCheck(this, PrimitiveBatch);
 
-    this._rectangleVertexData = [];
-    this._rectangleColorData = [];
-    this._rectangleCount = 0;
-
-    this._transformMatrix = new Matrix4();
-    this._rectangleData = new Float32Array([0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]);
-}
-
-PrimitiveBatch.prototype.unload = function () {
-    gl.deleteBuffer(this._vertexBuffer);
-    gl.deleteBuffer(this._colorBuffer);
-
-    this._primitiveShader.unload();
-};
-
-PrimitiveBatch.prototype.begin = function () {
-    //let gl = this._gl;
-    // bind buffers
-    //gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
-};
-
-PrimitiveBatch.prototype.clear = function () {
-    this._rectangleVertexData = [];
-    this._rectangleColorData = [];
-    this._rectangleCount = 0;
-};
-
-PrimitiveBatch.prototype.flush = function () {
-    var gl = this._gl;
-    var cameraMatrix = this._game.getActiveCamera().getMatrix();
-
-    this._game.getShaderManager().useShader(this._primitiveShader);
-
-    // draw rectangles?
-    if (this._rectangleCount > 0) {
-        // position buffer
-        gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, this._rectangleData, gl.STATIC_DRAW);
-
-        gl.enableVertexAttribArray(this._primitiveShader.attributes.aVertexPosition);
-        gl.vertexAttribPointer(this._primitiveShader.attributes.aVertexPosition, 2, gl.FLOAT, false, 0, 0);
-
-        // set uniforms
-        gl.uniformMatrix4fv(this._primitiveShader.uniforms.uMatrix._location, false, cameraMatrix);
-
-        for (var i = 0; i < this._rectangleCount; i++) {
-            this._transformMatrix.identity();
-            this._transformMatrix.translate([this._rectangleVertexData[i].x, this._rectangleVertexData[i].y, 0]);
-            this._transformMatrix.scale([this._rectangleVertexData[i].width, this._rectangleVertexData[i].height, 0]);
-
-            gl.uniformMatrix4fv(this._primitiveShader.uniforms.uTransform._location, false, this._transformMatrix.asArray());
-            gl.uniform4f(this._primitiveShader.uniforms.uColor._location, this._rectangleColorData[i].r, this._rectangleColorData[i].g, this._rectangleColorData[i].b, this._rectangleColorData[i].a);
-
-            gl.drawArrays(gl.TRIANGLES, 0, 6);
+        if (!isGame(game)) {
+            throw error("Cannot create primitive render, the Game object is missing from the parameters");
         }
+
+        // public properties:
+
+
+        // private properties:
+        this._game = game;
+        this._gl = game.getRenderContext().getContext();
+        this._primitiveShader = new PrimitiveShader();
+        this._vertexBuffer = this._gl.createBuffer();
+        this._colorBuffer = this._gl.createBuffer();
+
+        this._rectangleVertexData = [];
+        this._rectangleColorData = [];
+        this._rectangleCount = 0;
+
+        this._transformMatrix = new Matrix4();
+        this._rectangleData = new Float32Array([0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]);
     }
 
-    this.clear();
-};
+    //#endregion
 
-PrimitiveBatch.prototype.drawPoint = function (vector, size, color) {};
+    //#region Methods
 
-PrimitiveBatch.prototype.storeRectangle = function (rectangle, color) {
-    this._rectangleColorData.push(color);
-    this._rectangleVertexData.push(rectangle);
-    this._rectangleCount++;
-};
+    _createClass(PrimitiveBatch, [{
+        key: "unload",
+        value: function unload() {
+            this._gl.deleteBuffer(this._vertexBuffer);
+            this._gl.deleteBuffer(this._colorBuffer);
 
-PrimitiveBatch.prototype.drawLine = function (vectorA, vectorB, thickness, color) {};; /**
-                                                                                       * PrimitiveRender class for on demand direct drawing
-                                                                                       */
+            this._primitiveShader.unload();
+        }
+    }, {
+        key: "begin",
+        value: function begin() {
+            //let gl = this._gl;
+            // bind buffers
+            //gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
+        }
+    }, {
+        key: "clear",
+        value: function clear() {
+            this._rectangleVertexData = [];
+            this._rectangleColorData = [];
+            this._rectangleCount = 0;
+        }
+    }, {
+        key: "flush",
+        value: function flush() {
+            var gl = this._gl;
+            var cameraMatrix = this._game.getActiveCamera().getMatrix();
+
+            this._game.getShaderManager().useShader(this._primitiveShader);
+
+            // draw rectangles?
+            if (this._rectangleCount > 0) {
+                // position buffer
+                gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
+                gl.bufferData(gl.ARRAY_BUFFER, this._rectangleData, gl.STATIC_DRAW);
+
+                gl.enableVertexAttribArray(this._primitiveShader.attributes.aVertexPosition);
+                gl.vertexAttribPointer(this._primitiveShader.attributes.aVertexPosition, 2, gl.FLOAT, false, 0, 0);
+
+                // set uniforms
+                gl.uniformMatrix4fv(this._primitiveShader.uniforms.uMatrix._location, false, cameraMatrix);
+
+                for (var i = 0; i < this._rectangleCount; i++) {
+                    this._transformMatrix.identity();
+                    this._transformMatrix.translate([this._rectangleVertexData[i].x, this._rectangleVertexData[i].y, 0]);
+                    this._transformMatrix.scale([this._rectangleVertexData[i].width, this._rectangleVertexData[i].height, 0]);
+
+                    gl.uniformMatrix4fv(this._primitiveShader.uniforms.uTransform._location, false, this._transformMatrix.asArray());
+                    gl.uniform4f(this._primitiveShader.uniforms.uColor._location, this._rectangleColorData[i].r, this._rectangleColorData[i].g, this._rectangleColorData[i].b, this._rectangleColorData[i].a);
+
+                    gl.drawArrays(gl.TRIANGLES, 0, 6);
+                }
+            }
+
+            this.clear();
+        }
+    }, {
+        key: "drawPoint",
+        value: function drawPoint(vector, size, color) {}
+    }, {
+        key: "storeRectangle",
+        value: function storeRectangle(rectangle, color) {
+            this._rectangleColorData.push(color);
+            this._rectangleVertexData.push(rectangle);
+            this._rectangleCount++;
+        }
+    }, {
+        key: "drawLine",
+        value: function drawLine(vectorA, vectorB, thickness, color) {}
+
+        //#endregion
+
+    }]);
+
+    return PrimitiveBatch;
+}();
+
+; /**
+  * PrimitiveRender class for on demand direct drawing
+  */
 
 var PrimitiveRender = function () {
 
