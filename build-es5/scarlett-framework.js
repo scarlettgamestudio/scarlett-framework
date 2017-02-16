e4888cff2513c3974b992d0d9032a07bf4de75a9
Luís Fernandes <luisapidcloud@hotmail.com>
2017-02-16 01:59:25 +0000

    update: RigidBody to ES6

--- build-es5/scarlett-framework.js
+++ build-es5/scarlett-framework.js
@@ -12190,110 +12190,156 @@ var Vector4 = function () {
     return Vector4;
 }();
 
-;function RigidBody(params) {
-    params = params || {};
+; /**
+  *    RigidBody Class
+  */
 
-    // public properties
-    this.gameObject = null;
+var RigidBody = function () {
 
-    // private properties
-    this._isStatic = params.static || false;
-    this._mass = params.mass || null;
-    this._friction = params.friction || null;
-    this._body = null;
-}
+    //#region Constructors
 
-RigidBody.prototype._sync = function () {
-    var self = this;
+    /**
+     *
+     * @param params
+     */
+    function RigidBody(params) {
+        _classCallCheck(this, RigidBody);
 
-    if (!isObjectAssigned(this.gameObject)) {
-        return;
+        params = params || {};
+
+        // public properties
+        this.gameObject = null;
+
+        // private properties
+        this._isStatic = params.static || false;
+        this._mass = params.mass || null;
+        this._friction = params.friction || null;
+        this._body = null;
     }
 
-    if (!isObjectAssigned(this._body)) {
-        var pos = this.gameObject.transform.getPosition();
+    //#endregion
 
-        // TODO assign the body based on the object
-        var width = 1,
-            height = 1;
+    //#region Public Methods
 
-        if (isSprite(this.gameObject)) {
-            width = this.gameObject.getTexture().getWidth();
-            height = this.gameObject.getTexture().getHeight();
+    //#region Static Methods
+
+    //#endregion
+
+    _createClass(RigidBody, [{
+        key: "setMass",
+        value: function setMass(mass) {
+            this._mass = mass;
+            Matter.Body.setMass(this._body, this._mass);
+        }
+    }, {
+        key: "getMass",
+        value: function getMass() {
+            return this.mass;
+        }
+    }, {
+        key: "setGameObject",
+        value: function setGameObject(gameObject) {
+            this._sync();
+        }
+    }, {
+        key: "onGameObjectDetach",
+        value: function onGameObjectDetach() {
+            this.gameObject.transform.clearPositionGetter();
+            this.gameObject.transform.clearScaleGetter();
+            this.gameObject.transform.clearRotationGetter();
+        }
+    }, {
+        key: "onGameObjectPositionUpdated",
+        value: function onGameObjectPositionUpdated(value) {
+            if (isObjectAssigned(this._body)) {
+                Matter.Body.setPosition(this._body, value);
+            }
+        }
+    }, {
+        key: "onGameObjectRotationUpdated",
+        value: function onGameObjectRotationUpdated(value) {
+            if (isObjectAssigned(this._body)) {
+                Matter.Body.setAngle(this._body, value);
+            }
+        }
+    }, {
+        key: "onGameObjectScaleUpdated",
+        value: function onGameObjectScaleUpdated(value) {
+            if (isObjectAssigned(this._body)) {
+                Matter.Body.scale(this._body, value.x, value.y);
+            }
+        }
+    }, {
+        key: "unload",
+        value: function unload() {}
+        // TODO: do this
+
+
+        //#endregion
+
+        //#region Private Methods
+
+    }, {
+        key: "_sync",
+        value: function _sync() {
+            var self = this;
+
+            if (!isObjectAssigned(this.gameObject)) {
+                return;
+            }
+
+            if (!isObjectAssigned(this._body)) {
+                var pos = this.gameObject.transform.getPosition();
+
+                // TODO assign the body based on the object
+                var width = 1;
+                var height = 1;
+
+                if (isSprite(this.gameObject)) {
+                    width = this.gameObject.getTexture().getWidth();
+                    height = this.gameObject.getTexture().getHeight();
+                }
+
+                this._body = Matter.Bodies.rectangle(pos.x, pos.y, width, height, {
+                    isStatic: this._isStatic
+                });
+
+                Matter.World.add(GameManager.activeScene.getPhysicsWorld(), [this._body]);
+
+                var objScale = this.gameObject.transform.getScale();
+                Matter.Body.scale(this._body, objScale.x, objScale.y);
+
+                this.gameObject.transform.overridePositionGetter(function () {
+                    return {
+                        x: self._body.position.x,
+                        y: self._body.position.y
+                    };
+                });
+
+                this.gameObject.transform.overrideRotationGetter(function () {
+                    return self._body.angle;
+                });
+            }
+
+            if (isObjectAssigned(this._mass)) {
+                Matter.Body.setMass(this._body, this._mass);
+            }
+
+            if (isObjectAssigned(this._friction)) {
+                this._body.friction = this._friction;
+            }
         }
 
-        this._body = Matter.Bodies.rectangle(pos.x, pos.y, width, height, {
-            isStatic: this._isStatic
-        });
+        //#endregion
 
-        Matter.World.add(GameManager.activeScene.getPhysicsWorld(), [this._body]);
+    }]);
 
-        var objScale = this.gameObject.transform.getScale();
-        Matter.Body.scale(this._body, objScale.x, objScale.y);
+    return RigidBody;
+}();
 
-        this.gameObject.transform.overridePositionGetter(function () {
-            return {
-                x: self._body.position.x,
-                y: self._body.position.y
-            };
-        });
-
-        this.gameObject.transform.overrideRotationGetter(function () {
-            return self._body.angle;
-        });
-    }
-
-    if (isObjectAssigned(this._mass)) {
-        Matter.Body.setMass(this._body, this._mass);
-    }
-
-    if (isObjectAssigned(this._friction)) {
-        this._body.friction = this._friction;
-    }
-};
-
-RigidBody.prototype.setMass = function (mass) {
-    this._mass = mass;
-    Matter.Body.setMass(this._body, this._mass);
-};
-
-RigidBody.prototype.getMass = function () {
-    return this.mass;
-};
-
-RigidBody.prototype.setGameObject = function (gameObject) {
-    this._sync();
-};
-
-RigidBody.prototype.onGameObjectDetach = function () {
-    this.gameObject.transform.clearPositionGetter();
-    this.gameObject.transform.clearScaleGetter();
-    this.gameObject.transform.clearRotationGetter();
-};
-
-RigidBody.prototype.onGameObjectPositionUpdated = function (value) {
-    if (isObjectAssigned(this._body)) {
-        Matter.Body.setPosition(this._body, value);
-    }
-};
-
-RigidBody.prototype.onGameObjectRotationUpdated = function (value) {
-    if (isObjectAssigned(this._body)) {
-        Matter.Body.setAngle(this._body, value);
-    }
-};
-
-RigidBody.prototype.onGameObjectScaleUpdated = function (value) {
-    if (isObjectAssigned(this._body)) {
-        Matter.Body.scale(this._body, value.x, value.y);
-    }
-};
-
-RigidBody.prototype.unload = function () {
-    // TODO: do this
-};; /**
-    * Content Object Class
-    */
+; /**
+  * Content Object Class
+  */
 
 var ContentObject =
 
--- build/scarlett-framework.js
+++ build/scarlett-framework.js
@@ -12092,110 +12092,136 @@ class Vector4 {
     //#endregion
 
 }
-;function RigidBody (params) {
-	params = params || {};
+;/**
+ *    RigidBody Class
+ */
+class RigidBody {
 
-	// public properties
-	this.gameObject = null;
+    //#region Constructors
 
-	// private properties
-	this._isStatic = params.static || false;
-	this._mass = params.mass || null;
-	this._friction = params.friction || null;
-	this._body = null;
+    /**
+     *
+     * @param params
+     */
+    constructor(params) {
+        params = params || {};
 
-}
+        // public properties
+        this.gameObject = null;
 
-RigidBody.prototype._sync = function() {
-	var self = this;
+        // private properties
+        this._isStatic = params.static || false;
+        this._mass = params.mass || null;
+        this._friction = params.friction || null;
+        this._body = null;
+    }
 
-	if(!isObjectAssigned(this.gameObject)) {
-		return;
-	}
+    //#endregion
 
-	if(!isObjectAssigned(this._body)) {
-		var pos = this.gameObject.transform.getPosition();
+    //#region Public Methods
 
-		// TODO assign the body based on the object
-		var width = 1,
-			height = 1;
-		
-		if(isSprite(this.gameObject)) {
-			width = this.gameObject.getTexture().getWidth();
-			height = this.gameObject.getTexture().getHeight();
-		}
+    //#region Static Methods
 
-		this._body = Matter.Bodies.rectangle(pos.x, pos.y, width, height,
-			{
-				isStatic: this._isStatic
-			});
+    //#endregion
 
-		Matter.World.add(GameManager.activeScene.getPhysicsWorld(), [this._body]);
+    setMass(mass) {
+        this._mass = mass;
+        Matter.Body.setMass(this._body, this._mass);
+    }
 
-		var objScale = this.gameObject.transform.getScale();
-		Matter.Body.scale(this._body, objScale.x, objScale.y);
+    getMass() {
+        return this.mass;
+    }
 
-		this.gameObject.transform.overridePositionGetter(function() {
-			return {
-				x: self._body.position.x,
-				y: self._body.position.y
-			}
-		});
+    setGameObject(gameObject) {
+        this._sync();
+    }
 
-		this.gameObject.transform.overrideRotationGetter(function() {
-			return self._body.angle;
-		});
-	}
+    onGameObjectDetach() {
+        this.gameObject.transform.clearPositionGetter();
+        this.gameObject.transform.clearScaleGetter();
+        this.gameObject.transform.clearRotationGetter();
+    }
 
-	if(isObjectAssigned(this._mass)) {
-		Matter.Body.setMass(this._body, this._mass);
-	}
+    onGameObjectPositionUpdated(value) {
+        if (isObjectAssigned(this._body)) {
+            Matter.Body.setPosition(this._body, value);
+        }
+    }
 
-	if(isObjectAssigned(this._friction)) {
-		this._body.friction = this._friction;
-	}
-};
+    onGameObjectRotationUpdated(value) {
+        if (isObjectAssigned(this._body)) {
+            Matter.Body.setAngle(this._body, value);
+        }
+    }
 
-RigidBody.prototype.setMass = function(mass) {
-	this._mass = mass;
-	Matter.Body.setMass(this._body, this._mass);
-};
+    onGameObjectScaleUpdated(value) {
+        if (isObjectAssigned(this._body)) {
+            Matter.Body.scale(this._body, value.x, value.y);
+        }
+    }
 
-RigidBody.prototype.getMass = function() {
-	return this.mass;
-};
+    unload() {
+        // TODO: do this
+    }
 
-RigidBody.prototype.setGameObject = function(gameObject) {
-	this._sync();
-};
+    //#endregion
 
-RigidBody.prototype.onGameObjectDetach = function() {
-	this.gameObject.transform.clearPositionGetter();
-	this.gameObject.transform.clearScaleGetter();
-	this.gameObject.transform.clearRotationGetter();
-};
+    //#region Private Methods
 
-RigidBody.prototype.onGameObjectPositionUpdated = function(value) {
-	if(isObjectAssigned(this._body)) {
-		Matter.Body.setPosition(this._body, value);
-	}
-};
+    _sync() {
+        let self = this;
 
-RigidBody.prototype.onGameObjectRotationUpdated = function(value) {
-	if(isObjectAssigned(this._body)) {
-		Matter.Body.setAngle(this._body, value);
-	}
-};
+        if (!isObjectAssigned(this.gameObject)) {
+            return;
+        }
 
-RigidBody.prototype.onGameObjectScaleUpdated = function(value) {
-	if(isObjectAssigned(this._body)) {
-		Matter.Body.scale(this._body, value.x, value.y);
-	}
-};
+        if (!isObjectAssigned(this._body)) {
+            let pos = this.gameObject.transform.getPosition();
 
-RigidBody.prototype.unload = function() {
-	// TODO: do this
-};;/**
+            // TODO assign the body based on the object
+            let width = 1;
+            let height = 1;
+
+            if (isSprite(this.gameObject)) {
+                width = this.gameObject.getTexture().getWidth();
+                height = this.gameObject.getTexture().getHeight();
+            }
+
+            this._body = Matter.Bodies.rectangle(pos.x, pos.y, width, height,
+                {
+                    isStatic: this._isStatic
+                });
+
+            Matter.World.add(GameManager.activeScene.getPhysicsWorld(), [this._body]);
+
+            let objScale = this.gameObject.transform.getScale();
+            Matter.Body.scale(this._body, objScale.x, objScale.y);
+
+            this.gameObject.transform.overridePositionGetter(function () {
+                return {
+                    x: self._body.position.x,
+                    y: self._body.position.y
+                }
+            });
+
+            this.gameObject.transform.overrideRotationGetter(function () {
+                return self._body.angle;
+            });
+        }
+
+        if (isObjectAssigned(this._mass)) {
+            Matter.Body.setMass(this._body, this._mass);
+        }
+
+        if (isObjectAssigned(this._friction)) {
+            this._body.friction = this._friction;
+        }
+    }
+
+    //#endregion
+
+};/**
  * Content Object Class
  */
 class ContentObject {
