b77b8c1b10ed2d5efaf6bb9f1ad8d80b7f0fef8f
João Alves <joao.cpp.sca@gmail.com>
2017-04-09 16:12:15 +0100

    FIX: objectify static array function UPDATE: build update

--- src/utility/objectify.js
+++ src/utility/objectify.js
@@ -21,7 +21,7 @@ class Objectify {
      * Objectify an array:
      * @param array
      */
-    array(array) {
+    static array(array) {
         let result = [];
         array.forEach(function (elem) {
             // this element has objectify implemented?
