dea557f55ef77887393ee85f6a11545325d72cdc
Luís Fernandes <luisapidcloud@hotmail.com>
2017-09-17 18:02:36 +0100

    chore(travis): add node 8 build

--- src/common/fileContext.js
+++ src/common/fileContext.js
@@ -21,20 +21,26 @@ export default class FileContext {
     let headers = {};
 
     // iterate through every header line
-    xhr.getAllResponseHeaders().split("\r\n").forEach(headerLine => {
-      let index = headerLine.indexOf(":");
+    xhr
+      .getAllResponseHeaders()
+      .split("\r\n")
+      .forEach(headerLine => {
+        let index = headerLine.indexOf(":");
 
-      // if ':' character does not exist,
-      // no need to go further on this iteration
-      if (index === -1) {
-        return;
-      }
+        // if ':' character does not exist,
+        // no need to go further on this iteration
+        if (index === -1) {
+          return;
+        }
 
-      let key = headerLine.slice(0, index).toLowerCase().trim();
-      let value = headerLine.slice(index + 1).trim();
+        let key = headerLine
+          .slice(0, index)
+          .toLowerCase()
+          .trim();
+        let value = headerLine.slice(index + 1).trim();
 
-      headers[key] = value;
-    });
+        headers[key] = value;
+      });
 
     return new FileContext(headers, xhr.responseText);
   }
