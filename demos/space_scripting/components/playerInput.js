51bca0b24c676c00dd5f055b848cd74110084835
whitebeard86 <joao.cpp.sca@gmail.com>
2017-09-19 22:12:30 +0100

    feat(scripts): scripts update

--- demos/space_scripting/components/playerInput.js
+++ demos/space_scripting/components/playerInput.js
@@ -18,29 +18,31 @@ class PlayerInput extends SC.Script {
 
     if (keyboard.isKeyDown(Keys.RightArrow) || keyboard.isKeyDown(Keys.D)) {
       directionHorizontal = 1;
+
     } else if (keyboard.isKeyDown(Keys.LeftArrow) || keyboard.isKeyDown(Keys.A)) {
       directionHorizontal = -1;
     }
 
     if (keyboard.isKeyDown(Keys.UpArrow) || keyboard.isKeyDown(Keys.W)) {
       directionVertical = -1;
+
     } else if (keyboard.isKeyDown(Keys.DownArrow) || keyboard.isKeyDown(Keys.S)) {
       directionVertical = 1;
     }
 
-    this.gameObject.transform.translate(
+    this.transform.translate(
       this.strengthHorizontal * directionHorizontal * delta,
       this.strengthVertical * directionVertical * delta
     );
 
-    if (directionHorizontal == 0 && this.gameObject.transform.getPosition().x > -this._offsetBreakpoint) {
-      this.gameObject.transform.translate(this.fallback * delta * -1, 0);
+    if (directionHorizontal === 0 && this.transform.getPosition().x > -this._offsetBreakpoint) {
+      this.transform.translate(this.fallback * delta * -1, 0);
 
-      if (this.gameObject.transform.getPosition().x < -this._offsetBreakpoint) {
-        this.gameObject.transform.setPosition(-this._offsetBreakpoint, this.gameObject.transform.getPosition().y);
+      if (this.transform.getPosition().x < -this._offsetBreakpoint) {
+        this.transform.setPosition(-this._offsetBreakpoint, this.transform.getPosition().y);
       }
     }
   }
 }
 
-SC.Scripts.addScript("playerInput", PlayerInput);
+SC.addScript("playerInput", PlayerInput);
