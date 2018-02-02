9f74664564dc991dad950b49790695292fb3f755
Apidcloud <luisapidcloud@hotmail.com>
2018-02-02 17:32:29 +0000

    feat: WIP loadFontAsync

--- src/utility/fontLoader.js
+++ src/utility/fontLoader.js
@@ -5,6 +5,7 @@ import Path from "utility/path";
 import { FontSupportedTypes } from "utility/fontSupportedTypes";
 import FontStyle from "core/fontStyle";
 import BMFontParser from "utility/bmFontParser";
+import GenerateBMFont from "utility/generateBMFont";
 
 /**
  * FontLoader Utility Class
@@ -14,7 +15,10 @@ export default class FontLoader {
     return FontSupportedTypes.includes(extension);
   }
 
-  static async loadFontAsync(path: string, contentLoader: ContentLoader = ContentLoader): Promise<?FontStyle> {
+  static async validateFont(
+    path: string,
+    contentLoader: ContentLoader
+  ): Promise<?{ imagePath: string, specPath: string }> {
     const basename = Path.getBasename(path);
     const extension = Path.getFileExtension(basename);
 
@@ -28,13 +32,32 @@ export default class FontLoader {
     const imagePath = baseFilePath + ".png";
     const specPath = baseFilePath + ".json";
 
+    if (!await FontLoader.textureAndSpecExist(imagePath, specPath, contentLoader)) {
+      return null;
+    }
+    return { imagePath, specPath };
+  }
+
+  static async textureAndSpecExist(
+    imagePath: string,
+    specPath: string,
+    contentLoader: ContentLoader
+  ): Promise<boolean> {
     const textureExists: boolean = await contentLoader.fileExistsAsync(imagePath);
     const specExists: boolean = await contentLoader.fileExistsAsync(specPath);
 
-    // no need to go further if either texture or spec cannot be found
-    if (!textureExists || !specExists) {
+    return textureExists && specExists;
+  }
+
+  static async loadFontAsync(path: string, contentLoader: ContentLoader = ContentLoader): Promise<?FontStyle> {
+    const validationResult = await FontLoader.validateFont(path, contentLoader);
+
+    if (validationResult === null || !validationResult.imagePath || !validationResult.specPath) {
       return null;
     }
+
+    const imagePath = validationResult.imagePath;
+    const specPath = validationResult.specPath;
 
     const image = await contentLoader.loadImage(imagePath, imagePath);
     const specFileContext = await contentLoader.loadFile(specPath, specPath);
--- src/utility/generateBMFont.js
+++ src/utility/generateBMFont.js
@@ -5,13 +5,37 @@
  * Calling generate function will return null otherwise
  */
 export default class GenerateBMFont {
-  static generate() {
-    if (typeof window !== "undefined") {
+  static get BMFontOptions(): {} {
+    return {
+      outputType: "json",
+      distanceRange: 4,
+      smartSize: true,
+      pot: true
+    };
+  }
+
+  static generate(fontPath: string): ?boolean {
+    if (!window || !window.process || !window.process.type) {
       console.warn("This module cannot be used in the browser. Use through node instead.");
       return null;
     }
 
-    console.warn("node use!!!");
-    return true;
+    const generateBMFont = require("msdf-bmfont-xml");
+    const fs = require("fs");
+
+    return generateBMFont(fontPath, GenerateBMFont.BMFontOptions, (error, textures, font) => {
+      if (error) throw error;
+
+      textures.forEach(texture => {
+        fs.writeFile(texture.filename + ".png", texture.texture, err => {
+          if (err) throw err;
+        });
+      });
+      fs.writeFile(font.filename, font.data, err => {
+        if (err) throw err;
+      });
+
+      return true;
+    });
   }
 }
