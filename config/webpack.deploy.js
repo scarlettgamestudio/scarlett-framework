edb08ab45b3576e6f1ad53d7005edfe212a4774e
Apidcloud <luisapidcloud@hotmail.com>
2018-02-15 00:11:07 +0000

    chore: remove flow-runtime from dist production build

--- config/webpack.deploy.js
+++ config/webpack.deploy.js
@@ -5,10 +5,45 @@ const relativeOutputPath = "build/dist";
 // package name to an ES6 minified version
 const finalPackageName = packageName + ".browser.min.js";
 
+const loaderSetup = {
+  loader: "babel-loader",
+  options: {
+    plugins: [
+      "transform-runtime",
+      "lodash"
+    ],
+    presets: [
+      [
+        "env",
+        {
+          targets: {
+            browsers: ["last 2 versions"]
+          },
+          modules: false
+        }
+      ],
+      "flow",
+      "stage-3"
+    ],
+    retainLines: true
+  }
+};
+
 module.exports = {
   output: {
     path: path.resolve(relativeOutputPath),
     publicPath: relativeOutputPath,
     filename: finalPackageName
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // Skip any files outside of `src` directory
+        include: /src/,
+        exclude: /node_modules/,
+        use: loaderSetup
+      }
+    ]
   }
 };
